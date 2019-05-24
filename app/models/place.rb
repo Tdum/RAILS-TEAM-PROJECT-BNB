fc3713812597c40b8d440577a4c143dd9057ad0f
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-24 18:01:20 +0200

    add photos with json format to table places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  mount_uploaders :photos, PhotoUploader
+  serialize :avatars, JSON # If you use SQLite, add this line.
+
 end
