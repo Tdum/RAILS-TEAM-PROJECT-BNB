3ee4055cc3afa4ac9c9522a4c87571f8daeeaccf
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-24 18:06:05 +0200

    fix

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,4 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :bookings
   mount_uploaders :photos, PhotoUploader
-  serialize :avatars, JSON # If you use SQLite, add this line.
-
 end
