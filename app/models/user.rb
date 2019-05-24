a525f47463905b042d7600b8e79f864ed7f797cc
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-05-24 15:55:32 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bookings, through: :places
+  has_many :places, through: :bookings
   mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
