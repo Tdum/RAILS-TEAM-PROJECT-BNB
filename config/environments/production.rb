318f8afbdf78ec70cba0a90effaa2fcf79ed5f99
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-27 16:04:50 +0200

    fix Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
+
+  config.action_mailer.default_url_options = { host: 'https://morning-sands-64518.herokuapp.com', port: 3000 }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
