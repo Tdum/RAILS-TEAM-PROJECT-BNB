28b0790ed94bb4e70ef63ac9f60ac39dad3bbf99
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-05-27 14:21:05 +0200

    routes created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def place_params
+    params.require(:booking).permit(:guests, :date, :booking_type)
+  end
+
+  def place_find
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,34 @@
 class PlacesController < ApplicationController
+  before_action :place_find, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:address, :party_type, :name, :description, :guest_capacity, :price, :photo, :lattitude, :longitude)
+  end
+
+  def place_find
+    @place = Place.find(params[:id])
+  end
 end
