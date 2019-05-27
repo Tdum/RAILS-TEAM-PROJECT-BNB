25ab95eaa17ee81fc758b251572e080810431a18
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-27 09:18:01 +0200

    add longitude & latitude to Place model and config model to use geocoder gem

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,6 @@ class Place < ApplicationRecord
   has_many :bookings
   validates :name, :address, :description, :guest_capacity, :price, presence: true
   mount_uploaders :photos, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
