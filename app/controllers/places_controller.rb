1711fb419d6a9e80c5c29f2f9ffab63abb38c3cd
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-05-28 09:15:39 +0200

    place creation form designed

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,9 +9,17 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    @place.user_id = current_user.id
+    if @place.save
+      redirect_to place_path(@place)
+    else
+      render 'places/new'
+    end
   end
 
   def edit
