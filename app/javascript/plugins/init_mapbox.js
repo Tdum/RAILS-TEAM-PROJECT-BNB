66546fcc74b613a0e73f70f0fa057356d26c29ce
Thibaut Dumontet <dumontet.thibaut@gmail.com>
2019-05-28 17:00:32 +0200

    add mapbox but buggy

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,19 +2,19 @@ import mapboxgl from 'mapbox-gl';
 
 const mapElement = document.getElementById('map');
 
-const initMapbox = () => {
-
-  const fitMapToMarkers = (map, markers) => {
+const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
+
+const initMapbox = () => {
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v11'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
