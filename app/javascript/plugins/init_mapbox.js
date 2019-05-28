1ed8161483a1133cf656a44a15e93974e24212f4
Tolan Thornton <taoantaoan@hotmail.com>
2019-05-28 23:18:51 +0200

    minor refacto tweaks

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const addMarkers = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 50, maxZoom: 15 });
 };
 
 
