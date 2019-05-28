bc237e9a51e14fc86cd1dbc03adea675fb6ac506
Tolan Thornton <taoantaoan@hotmail.com>
2019-05-28 14:29:46 +0200

    Add map v2

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,4 +20,52 @@ const initMapbox = () => {
 };
 
 
+// const initMapbox = () => {
+
+//   if (mapElement) { // only build a map if there's a div#map to inject into
+//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+//     const map = new mapboxgl.Map({
+//       container: 'map',
+//       style: 'mapbox://styles/mapbox/streets-v10'
+//     });
+//     const markers = JSON.parse(mapElement.dataset.markers);
+//     markers.forEach((marker) => {
+//       new mapboxgl.Marker()
+//         .setLngLat([ marker.lng, marker.lat ])
+//         .addTo(map);
+//     });
+
+
+//     map.on('load', function() {
+// map.loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png', function(error, image) {
+// if (error) throw error;
+// map.addImage('cat', image);
+// map.addLayer({
+// "id": "points",
+// "type": "symbol",
+// "source": {
+// "type": "geojson",
+// "data": {
+// "type": "FeatureCollection",
+// "features": [{
+// "type": "Feature",
+// "geometry": {
+// "type": "Point",
+// "coordinates": [0, 0]
+// }
+// }]
+// }
+// },
+// "layout": {
+// "icon-image": "cat",
+// "icon-size": 0.25
+// }
+// });
+// });
+// });
+
+//   }
+// };
+
+
 export { initMapbox };
