b90fcd88bbfc8a586650668394cec14a43a7922a
Cyril Gaitte <cyril.gaitte@gmail.com>
2019-05-29 16:22:51 +0200

    fix done

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 
+if (document.getElementById('reviews')) {
+
 flatpickr(".datepicker", {
   disable: gon.dates,
   dateFormat: "Y-m-d"
 })
+}
 
 
